efc99fca175236c288db2bb4b20bfd774d83bb0b
Calu Marquez <carlamarquez.distextil@gmail.com>
2022-12-15 16:17:13 -0300

    home terminado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,103 @@
 import React from "react";
-
+import './footer.css'
+import {Container, Row, Col, ListGroup, ListGroupItem} from 'reactstrap';
+import {Link} from "react-router-dom";
+import logo from '../../assets/images/eco-logo.png';
 const Footer = ()=>{
     return (
-        <div>
-            Footer
-        </div>
+      <footer className="footer">
+        <Container>
+            <Row>
+                <Col lg='4' className="mb-4">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                    <div>
+                        <h1 className="text-white">Multimart</h1>
+                    </div>
+                </div>
+                <p className="footer__text mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet.</p>
+                </Col>
+
+                <Col lg='3' className="mb-4">
+                <div className="footer__quick-links">
+                    <h4 className="quick__links-title">Top Categories</h4>
+                    <ListGroup className="mb-3">
+                        <ListGroupItem className="ps-0 border-0">
+                            <Link to='#'>Mobile Phones</Link>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0">
+                            <Link to='#'>Modern Sofa</Link>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0">
+                            <Link to='#'>Arm Chair</Link>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0">
+                            <Link to='#'>Smart Watches</Link>
+                        </ListGroupItem>
+                    </ListGroup>
+                </div>
+                </Col>
+
+                <Col lg='2' className="mb-4">
+                 <div className="footer__quick-links">
+                    <h4 className="quick__links-title">Useful Links</h4>
+                    <ListGroup className="mb-3">
+                        <ListGroupItem className="ps-0 border-0">
+                            <Link to='/shop'>Shop</Link>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0">
+                            <Link to='/cart'>Cart</Link>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0">
+                            <Link to='/login'>Login</Link>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0">
+                            <Link to='#'>Privacy Policy</Link>
+                        </ListGroupItem>
+                    </ListGroup>
+                </div>
+                </Col>
+
+                <Col lg='3' className="mb-4">
+                
+                <div className="footer__quick-links">
+                    <h4 className="quick__links-title">Contact</h4>
+                    <ListGroup className="mb-3 d-flex">
+                        <ListGroupItem className="ps-0 border-0 d-flex">
+                            <span className="calu"><i class="ri-map-pin-line"></i></span>
+                            <p>Buenos Aires, Argentina</p>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0 d-flex">
+                        <span className="calu"><i class="ri-mail-line"></i></span>
+                            <p>carlamarquez.distextil@gmail.com</p>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0 d-flex">
+                            <span className="calu"><i class="ri-linkedin-fill"></i></span>
+                         <a href='https://www.linkedin.com/in/carla-marquez-/'><p>Linkedin</p></a>
+                        </ListGroupItem>
+
+                        <ListGroupItem className="ps-0 border-0 d-flex">
+                        <span className="calu"><i class="ri-github-fill"></i></span>
+                         <a href='https://github.com/caluMarquez'><p>GitHub</p></a>
+                        </ListGroupItem>
+
+
+                    </ListGroup>
+                </div>
+                
+
+                </Col>
+            </Row>
+        </Container>
+      </footer>
     )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.css';
 import {motion} from 'framer-motion';
@@ -22,10 +22,32 @@ const nav__links =[
 ]
 
 const Header = ()=>{
+
+    
+const headerRef = useRef(null);
+const menuRef = useRef(null);
+    const stickyHeaderFunc = ()=> {
+        window.addEventListener('scroll', ()=>{
+            if(document.body.scrollTop >80 || document.documentElement.scrollTop > 80){
+            headerRef.current.classList.add('sticky__header')}
+            else{
+            headerRef.current.classList.remove('sticky__header')
+            }
+        })
+    }
+
+    useEffect(()=>{
+        stickyHeaderFunc();
+        return ()=> window.removeEventListener('scroll', stickyHeaderFunc)
+        
+    })
+
+    const menuToggle =()=> menuRef.current.classList.toggle('active__menu')
     return(
-        <div> 
+       
+      
           
-            <header className='header'>
+            <header className='header' ref={headerRef}>
 
              <Container>
                 <Row>
@@ -35,10 +57,11 @@ const Header = ()=>{
                        <img src={logo} alt="logo"/> 
                         <div>  
                         <h1>Multimart</h1> 
-                        <p>Since 1994</p>
+                       
                     </div>
                     </div>
-                    <div className="navigation">
+                    
+                    <div className="navigation" ref={menuRef} onClick={menuToggle}>
                         <ul className="menu">
                  {
                     nav__links.map((item, index)=>(
@@ -59,24 +82,27 @@ const Header = ()=>{
 
                       <span className="cart__icon">
                       <i class="ri-shopping-bag-line"></i>
-                      <span className="badge">1</span>
+                      <span className="badge">2</span>
                         </span>  
 
                         <span>
                             <motion.img whileTap={{scale:1.2}} src={userIcon} alt=""/>
                         </span>
-                    </div>
 
-                    <div className="mobile__menu">
-                        <span>
-                        <i class="ri-menu-line"></i>
+                         <div className="mobile__menu">
+                        <span onClick={menuToggle}>
+                        <i class="ri-menu-line "></i>
                         </span>
                     </div>
+
+                    </div>
+
+                   
                     </div> 
                 </Row>
             </Container>  
             </header>
-        </div>
+           
     )
 }
 
