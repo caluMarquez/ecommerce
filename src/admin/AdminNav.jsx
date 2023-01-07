c9606365276ecdd5f2fba9b625fa27acf584ba72
Calu Marquez <carlamarquez.distextil@gmail.com>
2023-01-07 14:10:18 -0300

    dashboard

--- src/admin/AdminNav.jsx
+++ src/admin/AdminNav.jsx
@@ -1,14 +1,38 @@
 
 import React from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import {Container, Row} from 'reactstrap';
 import useAuth from '../custom-hooks/useAuth';
 import '../styles/admin-nav.css'
+import { NavLink } from 'react-router-dom';
+
+const admin__nav =[
+    {
+        display: 'Dashboard',
+        path: '/dashboard'
+    },
+    {
+        display: 'All-Products',
+        path: '/dashboard/all-products'
+    },
+    {
+        display: 'Orders',
+        path: '/dashboard/orders'
+    },
+    {
+        display: 'Users',
+        path: '/dashboard/users'
+    },
+    
+]
+
+
 
 const AdminNav = ()=>{
 
 const {currentUser} = useAuth()
 console.log("currentUser: ",currentUser);
     return (
+      <>
        <header className='admin__header'>
         <div className="admin__nav-top">
             <Container>
@@ -25,12 +49,35 @@ console.log("currentUser: ",currentUser);
                     <div className="admin__nav-top-right">
                         <span><i class="ri-notification-3-line"></i></span>
                         <span><i class="ri-user-settings-line"></i></span>
-                        <img src={currentUser.photoURL} alt=""/>
+                        <img src={currentUser && currentUser.photoURL} alt=""/>
                     </div>
                 </div>
             </Container>
         </div>
        </header>
+
+       <section className='admin__menu p-0'>
+
+        <Container>
+            <Row>
+                <div className="admin__navigation">
+                <ul className="admin__menu-list">
+                    {
+                        admin__nav.map((item, index)=>(
+                        <li key={index} className="admin__menu-item">
+                            <NavLink to={item.path} className={navClass=> navClass.isActive ? 'active__admin-menu' : ''} >{item.display}</NavLink>
+                        </li> ))
+                    }
+                </ul>
+
+                </div>
+            </Row>
+        </Container>
+
+       </section>
+      
+      </>
+
     )
 }
 
--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,12 +1,40 @@
 
 import React from 'react';
-
+import {Container,Row,Col} from 'reactstrap';
+import '../styles/dashboard.css';
 const Dashboard = ()=>{
     return (
         <>
-        <div>
-            HOLA
-        </div>
+        <section>
+            <Container>
+                <Row>
+                    <Col className="lg-3">
+                        <div className="revenue__box">
+                            <h5>Total Sales</h5>
+                            <span>$777</span>
+                        </div>
+                    </Col>
+                    <Col className="lg-3">
+                        <div className="order__box">
+                            <h5>Orders</h5>
+                            <span>$456</span>
+                        </div>
+                    </Col>
+                    <Col className="lg-3">
+                        <div className="products__box">
+                            <h5>Total products</h5>
+                            <span>Num</span>
+                        </div>
+                    </Col>
+                    <Col className="lg-3">
+                        <div className="users__box">
+                            <h5>Total Users</h5>
+                            <span>Num</span>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
         </>
     )
 }
